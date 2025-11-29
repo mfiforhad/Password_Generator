dd77544f0dfee975c7903c1f905ff5b503217142
Forhad <md.foredul.islam.forhad@gmail.com>
2025-11-29 23:55:58 +0600

    error handeling added

--- password_generator.py
+++ password_generator.py
@@ -1,29 +1,34 @@
 import random
+import secrets
 import string
 
-"""
-User Input to choose what kind of password needs
+print("choose an option to generate password")
+print("Press 1 : Low")
+print("Press 2 : Medium")
+print("Press 3 : Hard")
 
-"""
-print("choose options to generate password\nOption 1: Low\nOption 2: Medium\nOption 3: Hard")
+user_choice = input("Choose: ")
 
-user_choice = int(input("> "))
-password_limit = int(input("Limits: "))
+if user_choice not in ("1", "2", "3"):
+    print("Please enter a number between 1/2/3")
+    exit()
 
-if user_choice == 1:
-    low_password = "".join(set(random.choices(
-        string.ascii_letters, k=password_limit)))
-    print(
-        f"Your expected password for Option {user_choice}: Low \nPassword: {low_password}")
-elif user_choice == 2:
-    medium_password = "".join(set(random.choices(
-        (string.ascii_letters + string.digits), k=password_limit)))
-    print(
-        f"Your expected password for Option {user_choice}: Medium \nPassword: {medium_password}")
-elif user_choice == 3:
-    hard_password = "".join(set(random.choices(
-        (string.ascii_letters + string.digits + string.punctuation), k=password_limit)))
-    print(
-        f"Your expected password for Option {user_choice}: Hard \nPassword: {hard_password}")
-else:
-    print("Please choose between 1/2/3")
+password_limit = input("Limits: ")
+
+if not password_limit.isdigit():
+    print("Please enter a number")
+    exit()
+
+password_limit = int(password_limit)
+
+if user_choice == "1":
+    charecter = string.ascii_letters
+elif user_choice == "2":
+    charecter = string.ascii_letters + string.digits
+elif user_choice == "3":
+    charecter = string.ascii_letters + string.digits + string.punctuation
+
+password = "".join(secrets.choice(charecter) for _ in range(password_limit))  # generator expression
+
+print("Your Password:")
+print(password)
